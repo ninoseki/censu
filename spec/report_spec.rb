c69c08625a672ad7f05274dd5a88204eb39fc645
ninoseki <zgok0079@gmail.com>
2017-12-14 07:20:36 +0900

    Add test cases for websites & certificate report

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -8,7 +8,12 @@ describe CenSys::API do
   describe "#report" do
     context "ipv4" do
       it "should return ipv4 response" do
-        report = @api.report(:ipv4, query: "8.8.8.8", field: "location.country_code", bukets: 10)
+        params = {
+          query: "8.8.8.8",
+          field: "ports",
+          buckets: 10
+        }
+        report = @api.report(:ipv4, params)
         expect(report).to be_a(Report::Response)
         expect(report.status).to eq("ok")
         expect(report.ok?).to be_truthy
@@ -21,6 +26,34 @@ describe CenSys::API do
         expect(report.metadata.buckets).to be_a(Integer)
         expect(report.metadata.error_bound).to be_a(Integer)
         expect(report.metadata.query).to eq("8.8.8.8")
+      end
+    end
+
+    context "websites" do
+      it "should return website response" do
+        params = {
+          query: "google.com",
+          field: "ports",
+          buckets: 10
+        }
+        report = @api.report(:websites, params)
+        expect(report).to be_a(Report::Response)
+        expect(report.metadata).to be_a(Report::Metadata)
+        expect(report.metadata.query).to eq("google.com")
+      end
+    end
+
+    context "certificates" do
+      it "should return certificate response" do
+        params = {
+          query: "google.com",
+          field: "precert",
+          buckets: 10
+        }
+        report = @api.report(:certificates, params)
+        expect(report).to be_a(Report::Response)
+        expect(report.metadata).to be_a(Report::Metadata)
+        expect(report.metadata.query).to eq("google.com")
       end
     end
   end
